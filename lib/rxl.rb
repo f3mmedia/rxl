339ef0201b56e37c1f0f23708ed96e1ce9fde2bc
Ian McWilliams <ian.mcwilliams@f3mmedia.co.uk>
2018-04-21 14:36:49 +0100

    add read and write methods to rxl module

--- lib/rxl.rb
+++ lib/rxl.rb
@@ -1,5 +1,16 @@
 require 'rxl/version'
+require 'excel'
 
 module Rxl
+
+  def self.write_file(filepath, file_hash)
+    xl = Excel.new(file_hash)
+    xl.save_file(filepath)
+  end
+
+  def self.read_file(filepath)
+    xl = Excel.new(filepath)
+    xl.hash_workbook
+  end
 
 end
