d1ad40f78f7f9ae0193b04c3a0a244436c9ff48f
Ian McWilliams <ian.mcwilliams@f3mmedia.co.uk>
2018-04-21 14:50:17 +0100

    add source keyword to Excel calls

--- lib/rxl.rb
+++ lib/rxl.rb
@@ -4,12 +4,12 @@ require 'excel'
 module Rxl
 
   def self.write_file(filepath, file_hash)
-    xl = Excel.new(file_hash)
+    xl = Excel.new(source: file_hash)
     xl.save_file(filepath)
   end
 
   def self.read_file(filepath)
-    xl = Excel.new(filepath)
+    xl = Excel.new(source: filepath)
     xl.hash_workbook
   end
 
