7fc1fea6467ceac5e14c3f288fe00a7c74003f74
Ian McWilliams <ian.mcwilliams@f3mmedia.co.uk>
2018-04-21 13:57:25 +0100

    fix require path for excel class file

--- spec/support/env.rb
+++ spec/support/env.rb
@@ -2,6 +2,6 @@ require 'rspec'
 require 'fileutils'
 require 'pathname'
 require_relative 'excel_spec_helpers'
-require_relative '../../excel'
+require_relative '../../lib/excel'
 
 ENV['TEMP_XLSX_PATH'] = 'spec/temp_xlsx'
