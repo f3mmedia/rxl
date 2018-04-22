1b21227287af812d24803312a80a87bcf0c3e6cc
Ian McWilliams <ian.mcwilliams@f3mmedia.co.uk>
2018-04-22 17:57:45 +0100

    move value call inside parent key specification

--- lib/excel.rb
+++ lib/excel.rb
@@ -28,7 +28,7 @@ class Excel
     columns = cells.keys.map { |key| key[/\D+/] }.uniq
     cells.keys.map { |key| key[/\d+/] }.uniq.map do |row_number|
       columns[1..-1].each_with_object({}) do |column_letter, this_hash|
-        this_hash[cells["#{column_letter}1"]][:value] = cells["#{column_letter}#{row_number}"][:value]
+        this_hash[cells["#{column_letter}1"][:value]] = cells["#{column_letter}#{row_number}"][:value]
       end
     end
   end
