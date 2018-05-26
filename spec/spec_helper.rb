f9729ff047a81b588d433b9ae3ab16f49bda62cf
Ian McWilliams <ian.mcwilliams@f3mmedia.co.uk>
2018-05-26 11:53:56 +0100

    add json gem to spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,7 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'rxl'
 require 'rspec'
 require 'fileutils'
+require 'json'
 require 'pathname'
 require_relative 'support/rxl_spec_helpers'
 
