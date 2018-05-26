6f6046933688a047081e1b54c8eedceb74f23714
Ian McWilliams <ian.mcwilliams@f3mmedia.co.uk>
2018-05-26 14:50:26 +0100

    add initial write tests, amend filename key to filepath, refactor helper

--- spec/rxl_spec.rb
+++ spec/rxl_spec.rb
@@ -13,13 +13,13 @@ describe Rxl do
 
     it 'returns a hash_workbook with one empty hash_worksheet from an empty file' do
       RxlSpecHelpers.generate_test_excel_file(self, :empty_file)
-      read_hash = Rxl.read_file(RxlSpecHelpers.test_data(:filename, :empty_file))
+      read_hash = Rxl.read_file(RxlSpecHelpers.test_data(:filepath, :empty_file))
       expect(read_hash).to eq(RxlSpecHelpers.test_data(:expected_hash, :empty_file))
     end
 
     it 'reads in worksheet names' do
       RxlSpecHelpers.generate_test_excel_file(self, :worksheet_names)
-      read_hash = Rxl.read_file(RxlSpecHelpers.test_data(:filename, :worksheet_names))
+      read_hash = Rxl.read_file(RxlSpecHelpers.test_data(:filepath, :worksheet_names))
       expect(read_hash).to eq(RxlSpecHelpers.test_data(:expected_hash, :worksheet_names))
     end
 
@@ -30,49 +30,49 @@ describe Rxl do
   context 'reads cell raw values' do
 
     it 'with string input as String :text regardless of number format' do
-      read_hash = Rxl.read_file(RxlSpecHelpers.test_data(:filename, :cell_values_and_formats))
+      read_hash = Rxl.read_file(RxlSpecHelpers.test_data(:filepath, :cell_values_and_formats))
       cell_range = read_hash['string'][:cells].select { |key, _| key[/^B[2-7]$/] }
       expect(cell_range).to eq(RxlSpecHelpers.test_data(:expected_hash, :cell_raw_string_read))
     end
 
     it 'with whole number input as String :text for text number format, as FixNum :number for general/number number formats' do
-      read_hash = Rxl.read_file(RxlSpecHelpers.test_data(:filename, :cell_values_and_formats))
+      read_hash = Rxl.read_file(RxlSpecHelpers.test_data(:filepath, :cell_values_and_formats))
       cell_range = read_hash['number'][:cells].select { |key, _| key[/^B[2-4]$/] }
       expect(cell_range).to eq(RxlSpecHelpers.test_data(:expected_hash, :cell_raw_number_read))
     end
 
     it 'with float input as String :text for text number format, as FixNum :number for number number format' do
-      read_hash = Rxl.read_file(RxlSpecHelpers.test_data(:filename, :cell_values_and_formats))
+      read_hash = Rxl.read_file(RxlSpecHelpers.test_data(:filepath, :cell_values_and_formats))
       cell_range = read_hash['float'][:cells].select { |key, _| key[/^B[3-4]$/] }
       expect(cell_range).to eq(RxlSpecHelpers.test_data(:expected_hash, :cell_raw_float_read))
     end
 
     it 'with date input as String :text for text/percentage format, as DateTime :date for time/date number formats' do
-      read_hash = Rxl.read_file(RxlSpecHelpers.test_data(:filename, :cell_values_and_formats))
+      read_hash = Rxl.read_file(RxlSpecHelpers.test_data(:filepath, :cell_values_and_formats))
       cell_range = read_hash['date'][:cells].select { |key, _| key[/^B(3|[5-7])$/] }
       expect(cell_range).to eq(RxlSpecHelpers.test_data(:expected_hash, :cell_raw_date_read))
     end
 
     it 'with time input as String :text for text/percentage format, as DateTime :time for time number format' do
-      read_hash = Rxl.read_file(RxlSpecHelpers.test_data(:filename, :cell_values_and_formats))
+      read_hash = Rxl.read_file(RxlSpecHelpers.test_data(:filepath, :cell_values_and_formats))
       cell_range = read_hash['time'][:cells].select { |key, _| key[/^B(3|[6-7])$/] }
       expect(cell_range).to eq(RxlSpecHelpers.test_data(:expected_hash, :cell_raw_time_read))
     end
 
     it 'with percentage input as String :text for text format, as FixNum :number for percentage number format' do
-      read_hash = Rxl.read_file(RxlSpecHelpers.test_data(:filename, :cell_values_and_formats))
+      read_hash = Rxl.read_file(RxlSpecHelpers.test_data(:filepath, :cell_values_and_formats))
       cell_range = read_hash['percentage'][:cells].select { |key, _| key[/^B(3|7)$/] }
       expect(cell_range).to eq(RxlSpecHelpers.test_data(:expected_hash, :cell_raw_percentage_read))
     end
 
     it 'with percentage float input as String :text for text format, as Float :number for percentage number format' do
-      read_hash = Rxl.read_file(RxlSpecHelpers.test_data(:filename, :cell_values_and_formats))
+      read_hash = Rxl.read_file(RxlSpecHelpers.test_data(:filepath, :cell_values_and_formats))
       cell_range = read_hash['percentage_float'][:cells].select { |key, _| key[/^B(3|7)$/] }
       expect(cell_range).to eq(RxlSpecHelpers.test_data(:expected_hash, :cell_raw_percentage_float_read))
     end
 
     it 'with empty input as NilClass :general regardless of number format' do
-      read_hash = Rxl.read_file(RxlSpecHelpers.test_data(:filename, :cell_values_and_formats))
+      read_hash = Rxl.read_file(RxlSpecHelpers.test_data(:filepath, :cell_values_and_formats))
       cell_range = read_hash['empty'][:cells].select { |key, _| key[/^B[2-7]$/] }
       expect(cell_range).to eq(RxlSpecHelpers.test_data(:expected_hash, :cell_raw_empty_read))
     end
@@ -82,45 +82,89 @@ describe Rxl do
   context 'reads cell formula values' do
 
     it 'with string result as String :text regardless of number format, and collects formula' do
-      read_hash = Rxl.read_file(RxlSpecHelpers.test_data(:filename, :cell_values_and_formats))
+      read_hash = Rxl.read_file(RxlSpecHelpers.test_data(:filepath, :cell_values_and_formats))
       cell_range = read_hash['string'][:cells].select { |key, _| key[/^C[2-7]$/] }
       expect(cell_range).to eq(RxlSpecHelpers.test_data(:expected_hash, :cell_formula_string_read))
     end
 
     it 'with whole number result as String :text for text number format, as FixNum :number for general/number number formats, and collects formula' do
-      read_hash = Rxl.read_file(RxlSpecHelpers.test_data(:filename, :cell_values_and_formats))
+      read_hash = Rxl.read_file(RxlSpecHelpers.test_data(:filepath, :cell_values_and_formats))
       cell_range = read_hash['number'][:cells].select { |key, _| key[/^C[2-4]$/] }
       expect(cell_range).to eq(RxlSpecHelpers.test_data(:expected_hash, :cell_formula_number_read))
     end
 
     it 'with float result as String :text for text number format, as FixNum :number for number number format, and collects formula' do
-      read_hash = Rxl.read_file(RxlSpecHelpers.test_data(:filename, :cell_values_and_formats))
+      read_hash = Rxl.read_file(RxlSpecHelpers.test_data(:filepath, :cell_values_and_formats))
       cell_range = read_hash['float'][:cells].select { |key, _| key[/^C[3-4]$/] }
       expect(cell_range).to eq(RxlSpecHelpers.test_data(:expected_hash, :cell_formula_float_read))
     end
 
     it 'with date result as String :text for text number format, as DateTime :date for time/date number formats, and collects formula' do
-      read_hash = Rxl.read_file(RxlSpecHelpers.test_data(:filename, :cell_values_and_formats))
+      read_hash = Rxl.read_file(RxlSpecHelpers.test_data(:filepath, :cell_values_and_formats))
       cell_range = read_hash['date'][:cells].select { |key, _| key[/^C(3|5|6)$/] }
       expect(cell_range).to eq(RxlSpecHelpers.test_data(:expected_hash, :cell_formula_date_read))
     end
 
     it 'with time result as String :text for text format, as DateTime :time for time number format, and collects formula' do
-      read_hash = Rxl.read_file(RxlSpecHelpers.test_data(:filename, :cell_values_and_formats))
+      read_hash = Rxl.read_file(RxlSpecHelpers.test_data(:filepath, :cell_values_and_formats))
       cell_range = read_hash['time'][:cells].select { |key, _| key[/^C(3|6)$/] }
       expect(cell_range).to eq(RxlSpecHelpers.test_data(:expected_hash, :cell_formula_time_read))
     end
 
     it 'with percentage result as String :text for text format, as FixNum :number for percentage number format, and collects formula' do
-      read_hash = Rxl.read_file(RxlSpecHelpers.test_data(:filename, :cell_values_and_formats))
+      read_hash = Rxl.read_file(RxlSpecHelpers.test_data(:filepath, :cell_values_and_formats))
       cell_range = read_hash['percentage'][:cells].select { |key, _| key[/^C(3|7)$/] }
       expect(cell_range).to eq(RxlSpecHelpers.test_data(:expected_hash, :cell_formula_percentage_read))
     end
 
     it 'with percentage float result as String :text for text format, as Float :number for percentage number format, and collects formula' do
-      read_hash = Rxl.read_file(RxlSpecHelpers.test_data(:filename, :cell_values_and_formats))
+      read_hash = Rxl.read_file(RxlSpecHelpers.test_data(:filepath, :cell_values_and_formats))
       cell_range = read_hash['percentage_float'][:cells].select { |key, _| key[/^C(3|7)$/] }
       expect(cell_range).to eq(RxlSpecHelpers.test_data(:expected_hash, :cell_formula_percentage_float_read))
+    end
+
+  end
+
+  context 'when writing an excel file' do
+
+    it 'saves an empty hash as a file with the specified file name and a single empty sheet as "Sheet1"' do
+      Rxl.write_file(RxlSpecHelpers.test_data(:filepath, :empty_file), {})
+      expect(Pathname(RxlSpecHelpers.test_data(:filepath, :empty_file)).exist?).to eq(true)
+      read_hash = Rxl.read_file(RxlSpecHelpers.test_data(:filepath, :empty_file))
+      expect(read_hash.keys.length).to eq(1)
+      expect(read_hash.keys[0]).to eq('Sheet1')
+      expect(read_hash['Sheet1']).to eq({rows: {}, columns: {}, cells: {}})
+    end
+
+    it 'saves one or more sheets with the name specified and removes "Sheet1"' do
+      worksheet_name_arrays = [
+          ['worksheet_a'],
+          %w[a b c].map { |id| "worksheet_#{id}" },
+          ('a'..'dd').map { |id| "worksheet_#{id}" }
+      ]
+      worksheet_name_arrays.each do |worksheet_name_array|
+        hash_workbook_input = {}
+        worksheet_name_array.each { |worksheet_name| hash_workbook_input[worksheet_name] = {} }
+        Rxl.write_file(RxlSpecHelpers.test_data(:filepath, :empty_file), hash_workbook_input)
+        read_hash = Rxl.read_file(RxlSpecHelpers.test_data(:filepath, :empty_file))
+        expect(read_hash.keys.length).to eq(worksheet_name_array.length)
+        expect(read_hash.keys).to eq(worksheet_name_array)
+        worksheet_name_array.each do |worksheet_name|
+          expect(read_hash[worksheet_name]).to eq({rows: {}, columns: {}, cells: {}})
+        end
+      end
+    end
+
+    # MANUAL TESTS FOR SUCCESSFUL WRITE
+    # see the manual_tests directory for the raw ruby
+    # hashes and expected outcomes for writing each with Rxl
+
+    it 'returns an exception where the hash_workbook is not a hash' do
+      hash_workbook_inputs = [nil, '', 'abc', 0, [], ['a', 'b', 'c'], [1, 2, 3], {}.to_json]
+      hash_workbook_inputs.each do |hash_workbook_input|
+        exception = Rxl.write_file(RxlSpecHelpers.test_data(:filepath, :hash_validation), hash_workbook_input)
+        expect(exception.message).to eq(RxlSpecHelpers.test_data(:validation, :non_hash_workbook))
+      end
     end
 
   end
--- spec/support/rxl_spec_helpers.rb
+++ spec/support/rxl_spec_helpers.rb
@@ -20,103 +20,109 @@ module RxlSpecHelpers
   def self.test_data(type, key)
     temp_xlsx_path = ENV['TEMP_XLSX_PATH']
     return_value = {
-      filename: {
-          empty_file: "#{temp_xlsx_path}/empty_file.xlsx",
-          worksheet_names: "#{temp_xlsx_path}/worksheet_names.xlsx",
-          cell_values_and_formats: 'spec/support/static_test_files/cell_values_and_formats.xlsx'
-      }[key],
+      filepath: derived_filepath(key, temp_xlsx_path),
       write_hash: {
-          empty_file: {},
-          worksheet_names: {'test_a' => {}, 'test_b' => {}}
+        empty_file: {},
+        worksheet_names: {'test_a' => {}, 'test_b' => {}}
       }[key],
       expected_hash: {
-        empty_file: {'Sheet1'=>{rows: {}, columns: {}, cells: {}}},
+        empty_file: {'Sheet1' => {rows: {}, columns: {}, cells: {}}},
         worksheet_names: {
-            'test_a' => {rows: {}, columns: {}, cells: {}},
-            'test_b' => {rows: {}, columns: {}, cells: {}}
+          'test_a' => {rows: {}, columns: {}, cells: {}},
+          'test_b' => {rows: {}, columns: {}, cells: {}}
         },
         cell_raw_string_read: {
-            'B2' => {value: 'abcde', format: :text, formula: nil},
-            'B3' => {value: 'abcde', format: :text, formula: nil},
-            'B4' => {value: 'abcde', format: :text, formula: nil},
-            'B5' => {value: 'abcde', format: :text, formula: nil},
-            'B6' => {value: 'abcde', format: :text, formula: nil},
-            'B7' => {value: 'abcde', format: :text, formula: nil}
+          'B2' => {value: 'abcde', format: :text, formula: nil},
+          'B3' => {value: 'abcde', format: :text, formula: nil},
+          'B4' => {value: 'abcde', format: :text, formula: nil},
+          'B5' => {value: 'abcde', format: :text, formula: nil},
+          'B6' => {value: 'abcde', format: :text, formula: nil},
+          'B7' => {value: 'abcde', format: :text, formula: nil}
         },
         cell_raw_number_read: {
-            'B2' => {value: 12345, format: :number, formula: nil},
-            'B3' => {value: '12345', format: :text, formula: nil},
-            'B4' => {value: 12345, format: :number, formula: nil}
+          'B2' => {value: 12345, format: :number, formula: nil},
+          'B3' => {value: '12345', format: :text, formula: nil},
+          'B4' => {value: 12345, format: :number, formula: nil}
         },
         cell_raw_float_read: {
-            'B3' => {value: '123.45', format: :text, formula: nil},
-            'B4' => {value: 123.45, format: :number, formula: nil}
+          'B3' => {value: '123.45', format: :text, formula: nil},
+          'B4' => {value: 123.45, format: :number, formula: nil}
         },
         cell_raw_date_read: {
-            'B3' => {value: '01/01/2000', format: :text, formula: nil},
-            'B5' => {value: DateTime.parse('01/01/2000'), format: :date, formula: nil},
-            'B6' => {value: DateTime.parse('01/01/2000'), format: :date, formula: nil},
-            'B7' => {value: '01/01/2000%', format: :text, formula: nil}
+          'B3' => {value: '01/01/2000', format: :text, formula: nil},
+          'B5' => {value: DateTime.parse('01/01/2000'), format: :date, formula: nil},
+          'B6' => {value: DateTime.parse('01/01/2000'), format: :date, formula: nil},
+          'B7' => {value: '01/01/2000%', format: :text, formula: nil}
         },
         cell_raw_time_read: {
-            'B3' => {value: '10:15:30', format: :text, formula: nil},
-            'B6' => {value: DateTime.parse('31/12/1899 10:15:30'), format: :time, formula: nil},
-            'B7' => {value: '10:15:30%', format: :text, formula: nil}
+          'B3' => {value: '10:15:30', format: :text, formula: nil},
+          'B6' => {value: DateTime.parse('31/12/1899 10:15:30'), format: :time, formula: nil},
+          'B7' => {value: '10:15:30%', format: :text, formula: nil}
         },
         cell_raw_percentage_read: {
-            'B3' => {value: '100%', format: :text, formula: nil},
-            'B7' => {value: 1, format: :number, formula: nil}
+          'B3' => {value: '100%', format: :text, formula: nil},
+          'B7' => {value: 1, format: :number, formula: nil}
         },
         cell_raw_percentage_float_read: {
-            'B3' => {value: '123.45%', format: :text, formula: nil},
-            'B7' => {value: 1.2345, format: :number, formula: nil}
+          'B3' => {value: '123.45%', format: :text, formula: nil},
+          'B7' => {value: 1.2345, format: :number, formula: nil}
         },
         cell_raw_empty_read: {
-            'B2' => {value: nil, format: :general, formula: nil},
-            'B3' => {value: nil, format: :general, formula: nil},
-            'B4' => {value: nil, format: :general, formula: nil},
-            'B5' => {value: nil, format: :general, formula: nil},
-            'B6' => {value: nil, format: :general, formula: nil},
-            'B7' => {value: nil, format: :general, formula: nil}
+          'B2' => {value: nil, format: :general, formula: nil},
+          'B3' => {value: nil, format: :general, formula: nil},
+          'B4' => {value: nil, format: :general, formula: nil},
+          'B5' => {value: nil, format: :general, formula: nil},
+          'B6' => {value: nil, format: :general, formula: nil},
+          'B7' => {value: nil, format: :general, formula: nil}
         },
         cell_formula_string_read: {
-            'C2' => {value: 'abcde', format: :text, formula: 'CONCATENATE("abc","de")'},
-            'C3' => {value: 'abcde', format: :text, formula: 'CONCATENATE("abc","de")'},
-            'C4' => {value: 'abcde', format: :text, formula: nil},
-            'C5' => {value: 'abcde', format: :text, formula: nil},
-            'C6' => {value: 'abcde', format: :text, formula: 'CONCATENATE("abc","de")'},
-            'C7' => {value: 'abcde', format: :text, formula: nil}
+          'C2' => {value: 'abcde', format: :text, formula: 'CONCATENATE("abc","de")'},
+          'C3' => {value: 'abcde', format: :text, formula: 'CONCATENATE("abc","de")'},
+          'C4' => {value: 'abcde', format: :text, formula: nil},
+          'C5' => {value: 'abcde', format: :text, formula: nil},
+          'C6' => {value: 'abcde', format: :text, formula: 'CONCATENATE("abc","de")'},
+          'C7' => {value: 'abcde', format: :text, formula: nil}
         },
         cell_formula_number_read: {
-            'C2' => {value: 12345, format: :number, formula: '12340+5'},
-            'C3' => {value: '=12340+5', format: :text, formula: nil},
-            'C4' => {value: 12345, format: :number, formula: '12340+5'}
+          'C2' => {value: 12345, format: :number, formula: '12340+5'},
+          'C3' => {value: '=12340+5', format: :text, formula: nil},
+          'C4' => {value: 12345, format: :number, formula: '12340+5'}
         },
         cell_formula_float_read: {
-            'C3' => {value: '=123.41+0.04', format: :text, formula: nil},
-            'C4' => {value: 123.45, format: :number, formula: '123.41+0.04'}
+          'C3' => {value: '=123.41+0.04', format: :text, formula: nil},
+          'C4' => {value: 123.45, format: :number, formula: '123.41+0.04'}
         },
         cell_formula_date_read: {
-            'C3' => {value: '=DATE(2000,1,1)', format: :text, formula: nil},
-            'C5' => {value: DateTime.parse('01/01/2000'), format: :date, formula: 'DATE(2000,1,1)'},
-            'C6' => {value: DateTime.parse('01/01/2000'), format: :date, formula: 'DATE(2000,1,1)'}
+          'C3' => {value: '=DATE(2000,1,1)', format: :text, formula: nil},
+          'C5' => {value: DateTime.parse('01/01/2000'), format: :date, formula: 'DATE(2000,1,1)'},
+          'C6' => {value: DateTime.parse('01/01/2000'), format: :date, formula: 'DATE(2000,1,1)'}
         },
         cell_formula_time_read: {
-            'C3' => {value: '=TIME(10,15,30)', format: :text, formula: nil},
-            'C6' => {value: DateTime.parse('31/12/1899 10:15:30'), format: :time, formula: 'TIME(10,15,30)'}
+          'C3' => {value: '=TIME(10,15,30)', format: :text, formula: nil},
+          'C6' => {value: DateTime.parse('31/12/1899 10:15:30'), format: :time, formula: 'TIME(10,15,30)'}
         },
         cell_formula_percentage_read: {
-            'C3' => {value: '=50%+50%', format: :text, formula: nil},
-            'C7' => {value: 1, format: :number, formula: '50%+50%'}
+          'C3' => {value: '=50%+50%', format: :text, formula: nil},
+          'C7' => {value: 1, format: :number, formula: '50%+50%'}
         },
         cell_formula_percentage_float_read: {
-            'C3' => {value: '=123.41%+0.04%', format: :text, formula: nil},
-            'C7' => {value: 1.2345, format: :number, formula: '123.41%+0.04%'}
+          'C3' => {value: '=123.41%+0.04%', format: :text, formula: nil},
+          'C7' => {value: 1.2345, format: :number, formula: '123.41%+0.04%'}
         },
+      }[key],
+      validation: {
+        non_hash_workbook: 'hash_workbook must be a Hash'
       }[key]
     }[type]
     raise "no value found for type :#{type} and key :#{key}" unless return_value
     return_value
+  end
+
+  def self.derived_filepath(key, xlsx_path)
+    filepath = {
+      cell_values_and_formats: 'spec/support/static_test_files/cell_values_and_formats.xlsx'
+    }[key]
+    filepath || "#{xlsx_path}/#{key}.xlsx"
   end
 
 end
